604b5c965877064aa9b96ccc5b51f18d8586f7e2
KauanAltino <kauanaltino@gmail.com>
2024-10-31 22:24:35 -0300

    salvando projeto

--- app/src/test/java/com/cp3devops/app/AppApplicationTests.java
+++ app/src/test/java/com/cp3devops/app/AppApplicationTests.java
@@ -2,14 +2,10 @@ package com.cp3devops.app;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
-@ActiveProfiles("test")
-class AppApplicationTests {
-
-	@Test
-	void contextLoads() {
-	}
-
+public class AppApplicationTests {
+    @Test
+    void contextLoads() {
+    }
 }
